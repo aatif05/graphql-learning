5c0622677aebc0440cdacd1773b51f948e15e781
aatif05 <mohammedaatifrizwan@gmail.com>
2026-01-02 20:35:41 +0530

    Add files via upload

---
+++ server/src/test/java/com/aatif/graphql/learning/GraphqlLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aatif.graphql.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
